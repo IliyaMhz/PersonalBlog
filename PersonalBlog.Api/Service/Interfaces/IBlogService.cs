51e934f1a7ec39598c837c8b33da284028f9abc7
Iliya <mohammadzade.ilia.2006@gmail.com>
2025-09-26 23:04:24 +0330

    Added interface for Blog Service

---
+++ PersonalBlog.Api/Service/Interfaces/IBlogService.cs
@@ -0,0 +1,16 @@
+﻿using PersonalBlog.Api.Models.Dtos.BlogDtos;
+
+namespace PersonalBlog.Api.Service.Interfaces
+{
+    public interface IBlogService
+    {
+        Task<IEnumerable<BlogDto>> GetAllBlogsAsync();
+        Task<BlogDto> GetBlogByIdAsync(int id);
+        Task<BlogDto> CreateBlogAsync(CreateBlogDto createBlogDto);
+        Task<BlogDto> UpdateBlogAsync(int id, UpdateBlogDto updateBlogDto);
+        Task<bool> DeleteBlogAsync(int id);
+        Task<IEnumerable<BlogDto>> GetPublishedBlogsAsync();
+        Task<IEnumerable<BlogDto>> GetUnPublishedBlogsAsync();
+        Task<IEnumerable<BlogDto>> SearchBlogsAsync(string searchTerm);
+    }
+}
